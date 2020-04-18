d1ab7f01ba2cc1bca29361971cc814d7240fb8f9
DevOL78 <oleinik.dima098@mail.ru>
2020-04-19 01:11:56 +0300

    Multi-module app. First UI elements.

---
+++ engine/src/main/java/ru/hse/paramfunc/Engine.java
@@ -0,0 +1,88 @@
+package ru.hse.paramfunc;
+
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import ru.hse.paramfunc.engine.CameraBuilder;
+import ru.hse.paramfunc.engine.SpaceSubScene;
+import ru.hse.paramfunc.parser.FunctionValues3DParser;
+
+import java.io.IOException;
+import java.util.LinkedList;
+
+public class Engine {
+
+    private final static String TEST_FILE_PATH = "./etc/test13-04-2020_23-31-56.txt";
+
+    private static SpaceSubScene subScene;
+
+    public static void start(Scene scene) throws Exception {
+        loadPoints();
+
+        Group root = new Group();
+        subScene = new SpaceSubScene(1024, 726);
+        subScene.setUp();
+
+        setAnimationControls(scene);
+
+        CameraBuilder cameraBuilder = new CameraBuilder();
+        cameraBuilder.setUp(subScene, scene);
+
+        root.getChildren().add(subScene);
+
+        Pane spacePane = findSpacePane(scene);
+
+        if(spacePane != null) {
+            subScene.widthProperty().bind(spacePane.widthProperty());
+            subScene.heightProperty().bind(spacePane.heightProperty());
+            addRootToSpacePane(root, spacePane);
+        }
+    }
+
+    private static Pane findSpacePane(Scene scene) {
+        Pane sceneRoot = (Pane) scene.getRoot();
+        LinkedList<Pane> queue = new LinkedList<>();
+        queue.push(sceneRoot);
+
+        while (!queue.isEmpty()) {
+            Pane currentPane = queue.poll();
+            for (Node child : currentPane.getChildren()) {
+                if (child.getId() != null && child.getId().equals("spacePane")) {
+                    return (Pane) child;
+                } else if (child instanceof Pane) {
+                    queue.push((Pane) child);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private static void addRootToSpacePane(Group root, Pane pane) {
+        pane.getChildren().add(root);
+    }
+
+    private static void loadPoints() throws IOException {
+        FunctionValues3DParser.getInstance().parse(TEST_FILE_PATH);
+    }
+
+    private static void setAnimationControls(Scene scene) {
+        scene.setOnKeyPressed(event -> {
+            System.out.println(event.getCode());
+            if (event.getCode() == KeyCode.Z) {
+                subScene.startCurrentAnimation();
+            } else if (event.getCode() == KeyCode.X) {
+                subScene.stopCurrentAnimation();
+            } else if (event.getCode() == KeyCode.P) {
+                subScene.pauseCurrentAnimation();
+            } else if (event.getCode() == KeyCode.DIGIT1) {
+                subScene.setCurrentAnimation("Flying point");
+            } else if (event.getCode() == KeyCode.DIGIT2) {
+                subScene.setCurrentAnimation("Dynamic lines");
+            }
+        });
+    }
+
+}
--- ui/src/main/java/ru/hse/paramFunc/SceneRunner.java
+++ ui/src/main/java/ru/hse/paramFunc/SceneRunner.java
@@ -4,21 +4,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import ru.hse.paramfunc.Engine;
 import ru.hse.paramfunc.domain.enums.SceneType;
-
-import java.io.IOException;
 
 public class SceneRunner {
 
     private static final SceneRunner instance = new SceneRunner();
-    private SceneRunner() {}
+
+    private SceneRunner() {
+    }
+
     public static SceneRunner getInstance() {
         return instance;
     }
 
     private final static String STYLESHEET_PATH = "ru/hse/paramFunc/";
 
-    public void run(SceneType sceneType, Stage stage) throws IOException {
+    public void run(SceneType sceneType, Stage stage) throws Exception {
         switch (sceneType) {
             case MAIN: {
                 runMainScene(stage);
@@ -31,10 +33,11 @@ public class SceneRunner {
 
     }
 
-    private void runMainScene(Stage stage) throws IOException {
+    private void runMainScene(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        Scene scene = new Scene(root, 1024, 700);
+        Scene scene = new Scene(root, 1024, 700, true);
         scene.getStylesheets().add(STYLESHEET_PATH + "main.css");
+        Engine.start(scene);
         stage.setScene(scene);
         stage.show();
     }
