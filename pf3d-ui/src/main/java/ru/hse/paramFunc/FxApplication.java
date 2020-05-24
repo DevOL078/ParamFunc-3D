b3fae934e1b4a7350eb85595a9276d4f52b16d5e
DevOL78 <oleinik.dima098@mail.ru>
2020-05-24 23:36:11 +0300

    Display alert when settings haven't been loaded

--- pf3d-ui/src/main/java/ru/hse/paramFunc/FxApplication.java
+++ pf3d-ui/src/main/java/ru/hse/paramFunc/FxApplication.java
@@ -1,10 +1,14 @@
 package ru.hse.paramFunc;
 
 import javafx.application.Application;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import ru.hse.paramfunc.settings.AppSettings;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 
 public class FxApplication extends Application {
 
@@ -12,10 +16,20 @@ public class FxApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        AppSettings.init();
-        System.out.println("App settings have been loaded");
-        SceneRunner.getInstance().setMainStage(stage);
-        SceneRunner.getInstance().runMainScene();
+        try {
+            AppSettings.init();
+            System.out.println("App settings have been loaded");
+            SceneRunner.getInstance().setMainStage(stage);
+            SceneRunner.getInstance().runMainScene();
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Loading settings error");
+            alert.setHeaderText("File app.config hasn't been found.");
+            alert.setContentText("The config file must be into directory: " +
+                    Paths.get(".").toAbsolutePath().toString());
+            alert.initModality(Modality.WINDOW_MODAL);
+            alert.showAndWait();
+        }
     }
 
     public static void main(String[] args) {
